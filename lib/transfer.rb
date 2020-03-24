7c83ce2a36eec8c4ca5135f26433136eec58105d
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-24 19:58:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :status, :amount 
+
+def initialize (sender_account, receiver_amount, money)
+  @transfer = money
+  @sender = sender_account
+  @receiver = receiver_amount
+  @status = "pending"
+  @amount = 50 
+end
+
+def valid?
+  self.sender.valid? && self.receiver.valid?
+end 
+
+def execute_transaction 
+  if self.valid && sender.balance > @amount && self.status == "pending"
+    @sender.balance -= @amount 
+    @receiver.balance += @amount
+    @status = "complete"
+    
+  else 
+    @status = "rejection"
+    return "Transaction rejected. Please check your account balance."
+  end 
+end 
+
+def reverse_transfer 
+  if execute_transaction 
+    @sender.balance += @amount 
+    @receiver.balance -= @amount 
+    @staus = "reversed"
+end
+end
 end
